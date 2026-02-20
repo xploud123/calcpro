dd6afad036adee803ea7c40d763e14ce40f6bad3
xploud123 <96345599+xploud123@users.noreply.github.com>
2026-02-19 23:35:03 -0300

    Update auth.js

--- auth.js
+++ auth.js
@@ -2,20 +2,16 @@
 
 document.addEventListener("DOMContentLoaded", async () => {
 
-  // Aguarda o Supabase terminar de carregar a sessão
-  const { data: { session } } = await window.supabaseClient.auth.getSession();
+  const {
+    data: { session },
+  } = await window.supabaseClient.auth.getSession();
 
-  if (!session) {
-    // Pequeno delay para garantir restauração
-    setTimeout(async () => {
-      const { data: { session: retrySession } } =
-        await window.supabaseClient.auth.getSession();
+  if (session) return;
 
-      if (!retrySession) {
-        window.location.replace("login.html");
-      }
-    }, 300);
-
-  }
+  window.supabaseClient.auth.onAuthStateChange((event, session) => {
+    if (!session) {
+      window.location.replace("login.html");
+    }
+  });
 
 });
